36066d1319e1698571c686e30cefe1bbc338a127
Mark Mwima <markmwimar@gmail.com>
2024-05-26 21:51:15 +0300

    dynamic styling

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 const HomeCards = () => {
   return (
-    
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-                  <Card>
-                      <h2 className="text-2xl font-bold">For Developers</h2>
+          <Card>
+            <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
@@ -17,9 +16,10 @@ const HomeCards = () => {
             >
               Browse Jobs
             </a>
-            </Card>
-                  <Card>
-                       <h2 className="text-2xl font-bold">For Employers</h2>
+          </Card>
+
+          <Card bg="bg-indigo-100">
+            <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
@@ -33,7 +33,7 @@ const HomeCards = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeCards
+export default HomeCards;
