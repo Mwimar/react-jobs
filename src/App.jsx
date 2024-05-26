1a24430ffdb115f685f44cf5d0f7b4078bf8a14a
Mark Mwima <markmwimar@gmail.com>
2024-05-26 13:57:38 +0300

    adding lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 const App = () => {
+  const address = 'Nairobi';
+  const names=['Mathew', 'Mark', 'John']
   return (
+    <>
     <div className='text-5xl'>
       App
 
-    </div>
+      </div>
+      <p>welcome to { address}</p>
+      <p>Hello</p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name }</li>
+        ))}
+      </ul>
+    
+    </>
   )
 }
 
