67735bb3dd30d359ff2317c69b328b21a9ff1ab4
Mark Mwima <markmwimar@gmail.com>
2024-05-29 17:41:32 +0300

    loading effect

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -26,9 +26,15 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Recent Jobs" : "All Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            <>
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
